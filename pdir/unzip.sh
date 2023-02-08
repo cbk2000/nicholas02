ca58b4b40286ad235a16dcf2337bbf960bb4a3ae
Nicholas Sidharta <toxicancm@gmail.com>
2023-02-08 14:06:00 +0000

    Nicholas/Create input file, unzipping script

---
+++ pdir/unzip.sh
@@ -0,0 +1,16 @@
+INPUT = ../input
+OUTPUT = ../output
+
+if [! -d "$INPUT" ]; then
+	echo "Input directory doesn't exist, exiting status 1"
+	exit 1
+fi
+
+INPUT_FILE="${INPUT}/OS231 REGISTRATION.csv.zip"
+
+if [ ! -d "$OUTPUT" ]; then
+	echo "Output directory doesn't exist, creating output directory"
+	mkdir $OUTPUT
+fi
+
+unzip $INPUT -d $OUTPUT
